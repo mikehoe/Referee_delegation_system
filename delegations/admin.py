6f37ad147af4b766b63a85de1e6b08bd7f6e7397
Ondrej <ondra.hanus13@gmail.com>
2024-09-24 11:10:40 +0200

    delegations (app): admin setup

--- delegations/admin.py
+++ delegations/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-
 from delegations.models import Delegation
 
-admin.site.register(Delegation)
+
+class DelegationAdmin(admin.ModelAdmin):
+    list_display = ['match', 'referee', 'referee_role']
+    list_display_links = ['match', 'referee']
+    ordering = ['match__date_time']
+    search_fields = ['referee__name', 'referee__surname', 'match__code']
+
+admin.site.register(Delegation, DelegationAdmin)
