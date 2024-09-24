532bc5f8001ca7cde94ee7cc1d931284e6a58b7c
Ondrej <ondra.hanus13@gmail.com>
2024-09-24 09:10:19 +0200

    accounts and referees (app): fix admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from accounts.models import *
 
-# Register your models here.
+
+class ProfileRefereeAdmin(admin.ModelAdmin):
+    list_display = ['user', 'referee']
+    list_display_links = ['user', 'referee']
+    ordering = ['user__username']
+    search_fields = ['user__username', 'referee__name', 'referee__surname']
+
+
+class ProfileManagerAdmin(admin.ModelAdmin):
+    list_display = ['user']
+    list_display_links = ['user']
+    ordering = ['user__username']
+    search_fields = ['user__username']
+
+
+admin.site.register(ProfileReferee, ProfileRefereeAdmin)
+admin.site.register(ProfileManager, ProfileManagerAdmin)
