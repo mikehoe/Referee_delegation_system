3739255a8bc04f6694c0c889dd2338ae6867de4e
Ondrej <ondra.hanus13@gmail.com>
2024-09-26 09:19:09 +0200

    referees(app): forms and views update

--- referees/forms.py
+++ referees/forms.py
@@ -10,27 +10,13 @@ class RefereeForm(ModelForm):
         fields = '__all__'
 
     def clean_name(self):
-        cleaned_data = super().clean()
-        name = cleaned_data.get('name')
-        if name is None or len(name.strip()) == 0:
+        name = self.cleaned_data.get('name', '').strip()
+        if len(name) == 0:
             raise ValidationError('Please enter a name.')
-        return name.strip().capitalize()
+        return name.capitalize()
 
     def clean_surname(self):
-        cleaned_data = super().clean()
-        surname = cleaned_data.get('surname')
-        if surname is None or len(surname.strip()) == 0:
+        surname = self.cleaned_data.get('surname', '').strip()
+        if len(surname) == 0:
             raise ValidationError('Please enter a surname.')
-        return surname.strip().capitalize()
-
-    def clean(self):
-        cleaned_data = super().clean()
-        name = cleaned_data.get('name', '').strip()
-        surname = cleaned_data.get('surname', '').strip()
-
-        if len(name) == 0 and len(surname) == 0:
-            raise ValidationError('You must enter either a name or a surname.')
-
-        cleaned_data['name'] = name
-        cleaned_data['surname'] = surname
-        return cleaned_data
+        return surname.capitalize()
