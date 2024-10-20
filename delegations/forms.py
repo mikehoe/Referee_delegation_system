46b15750e56b691bfeac3063316e0457b7c76a7f
Ondrej <ondra.hanus13@gmail.com>
2024-10-21 00:09:45 +0200

    delegations(app): setup Delegation for 1 match.

--- delegations/forms.py
+++ delegations/forms.py
@@ -7,6 +7,101 @@ from delegations.models import Delegation, RefereeRole
 from referees.models import Referee
 
 
+class MatchDelegationForm(forms.Form):
+    referee_1R = forms.ModelChoiceField(
+        queryset=Referee.objects.none(),
+        required=False,
+        label='1.R'
+    )
+    referee_2R = forms.ModelChoiceField(
+        queryset=Referee.objects.none(),
+        required=False,
+        label='2.R'
+    )
+    referee_1L = forms.ModelChoiceField(
+        queryset=Referee.objects.none(),
+        required=False,
+        label='1.L'
+    )
+    referee_2L = forms.ModelChoiceField(
+        queryset=Referee.objects.none(),
+        required=False,
+        label='2.L'
+    )
+
+    def set_default_referee_names(self, match):
+        delegations = Delegation.objects.filter(match=match)
+        for delegation in delegations:
+            if delegation.referee_role == RefereeRole.FIRST_REFEREE:
+                self.fields['referee_1R'].initial = delegation.referee
+            elif delegation.referee_role == RefereeRole.SECOND_REFEREE:
+                self.fields['referee_2R'].initial = delegation.referee
+            elif delegation.referee_role == RefereeRole.FIRST_LINE_JUDGE:
+                self.fields['referee_1L'].initial = delegation.referee
+            elif delegation.referee_role == RefereeRole.SECOND_LINE_JUDGE:
+                self.fields['referee_2L'].initial = delegation.referee
+
+    def __init__(self, *args, match=None, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if match:
+            self.set_default_referee_names(match)
+
+            self.fields['referee_1R'].queryset = Delegation.get_available_referees(match, RefereeRole.FIRST_REFEREE)
+            self.fields['referee_2R'].queryset = Delegation.get_available_referees(match, RefereeRole.SECOND_REFEREE)
+            self.fields['referee_1L'].queryset = Delegation.get_available_referees(match, RefereeRole.FIRST_LINE_JUDGE)
+            self.fields['referee_2L'].queryset = Delegation.get_available_referees(match, RefereeRole.SECOND_LINE_JUDGE)
+
+            delegations = Delegation.objects.filter(match=match)
+            for delegation in delegations:
+                if delegation.referee_role == RefereeRole.FIRST_REFEREE:
+                    self.fields['referee_1R'].initial = delegation.referee
+                elif delegation.referee_role == RefereeRole.SECOND_REFEREE:
+                    self.fields['referee_2R'].initial = delegation.referee
+                elif delegation.referee_role == RefereeRole.FIRST_LINE_JUDGE:
+                    self.fields['referee_1L'].initial = delegation.referee
+                elif delegation.referee_role == RefereeRole.SECOND_LINE_JUDGE:
+                    self.fields['referee_2L'].initial = delegation.referee
+
+    def clean(self):
+        cleaned_data = super().clean()
+        match_id = self.data.get('match_id')
+
+        for role in ['1.R', '2.R', '1.L', '2.L']:
+            referee = cleaned_data.get(f'referee_{role}')
+            if referee:
+                if Delegation.objects.filter(match_id=match_id, referee=referee).exists():
+                    raise ValidationError(f"Referee {referee} is already delegated to this match.")
+
+                match = Match.objects.get(id=match_id)
+                if Delegation.objects.filter(match__date_time__date=match.date_time.date(), referee=referee).exists():
+                    raise ValidationError(
+                        f"Referee {referee} is already delegated on {match.date_time.date()} for another match.")
+
+    def save(self, match):
+        referees = {
+            '1.R': self.cleaned_data.get('referee_1R'),
+            '2.R': self.cleaned_data.get('referee_2R'),
+            '1.L': self.cleaned_data.get('referee_1L'),
+            '2.L': self.cleaned_data.get('referee_2L'),
+        }
+
+        for role, referee in referees.items():
+            if referee is not None:
+                # Trying to find existing delegation
+                existing_delegation = Delegation.objects.filter(match=match, referee_role=role).first()
+                if existing_delegation:
+                    # If exists, update it
+                    existing_delegation.referee = referee
+                    existing_delegation.save()
+                else:
+                    # If doesn’t exist, create new
+                    Delegation.objects.create(match=match, referee_role=role, referee=referee)
+            else:
+                # If referee is None ('----'), delete delegation
+                Delegation.objects.filter(match=match, referee_role=role).delete()
+
+
 class BulkDelegationForm(forms.Form):
     hidden = forms.ModelMultipleChoiceField(
         queryset=Match.objects.none(), widget=forms.HiddenInput()
--- delegations/views.py
+++ delegations/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from django.views import View
+
 from competitions.models import Match
 from delegations.models import Delegation
-from .forms import BulkDelegationForm
+from delegations.forms import BulkDelegationForm, MatchDelegationForm
+
+
+class MatchDelegationView(PermissionRequiredMixin, View):
+    permission_required = 'delegations.add_delegation'
+
+    def get(self, request, pk):
+        match = get_object_or_404(Match, id=pk)
+        form = MatchDelegationForm(match=match)
+        return render(request, 'match_delegation.html', {'form': form, 'match': match})
+
+    def post(self, request, pk):
+        match = get_object_or_404(Match, id=pk)
+        form = MatchDelegationForm(request.POST, match=match)
+
+        if form.is_valid():
+            form.save(match)
+
+            messages.success(request, 'Referees have been successfully delegated to the match.')
+
+            # Gets competition_in_season for given match
+            competition_in_season = match.competition_in_season
+
+            # create URL for redirect
+            redirect_url = reverse('matches_list', args=[competition_in_season.id])
+            return HttpResponseRedirect(redirect_url)
+
+        return render(request, 'match_delegation.html', {'form': form, 'match': match})
 
 
 def bulk_delegation_view(request):
